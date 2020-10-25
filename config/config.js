87a628fbf5b32004166e30b864235f9841a36be7
SwisoneQ <bartekramik115@gmail.com>
2020-10-25 10:47:37 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: "NzY4NTU5NTIwOTQyMTI5MjAy.X5CO1A.0oUhK90AboodRuKXwohETHDT_Ao",
+    token: "NzY4NTU5NTIwOTQyMTI5MjAy.X5CO1A.MC3LcDPijW3TUAaAyOohQHN1SCw",
     prefix: "/"
 }
